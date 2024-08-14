dc28a7836029535828fd03549e391cf3c3f3bf07
AKI <143816677+AkiHayashi1301@users.noreply.github.com>
2024-08-14 09:53:46 -0300

    versão 14/08/24 import time

--- yay.py
+++ yay.py
@@ -1,21 +1,30 @@
 import time
 
-tempo = 5
+tempo = 2
 
 nome =input ("What's your name?")
 
 time.sleep(tempo)
 print("How can i help you?")
+time.sleep(tempo)
 print("1 - exams")
+time.sleep(tempo)
 print("2 - consult")
+time.sleep(tempo)
 print("3 - Medic Prescription")
 
+time.sleep(tempo)
 opcao = int(input("Choose a number"))
+
 if opcao == 1:
+    time.sleep(tempo)
     print ("Write your CPF")
 
 elif opcao == 2:
+    time.sleep(tempo)
     print ("Write your CPF")
 
 else:
+    time.sleep(tempo)
     print("Try another number")
+
